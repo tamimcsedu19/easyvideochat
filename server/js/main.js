fc02a1f1a2b1e03e5f2e8ef8ed60d83f20e357a8
tamimcsedu19 <tamim.tamim1382@gmail.com>
2015-11-28 12:58:13 +0600

    Created function declarations

--- server/js/main.js
+++ server/js/main.js
@@ -1,28 +1,70 @@
 var isInitiator;
+username = ""
+while(true)
+{
+    username = prompt("Enter username:");
+    if (username !== "")
+        break;
 
-room = prompt("Enter room name:");
-
-var socket = io.connect();
-
-if (room !== "") {
-  console.log('Joining room ' + room);
-  socket.emit('create or join', room);
 }
 
-socket.on('full', function (room){
-  console.log('Room ' + room + ' is full');
+var socket = io.connect(); //Connecting to the server with a socket
+
+socket.emit('connected', username); // Invoking the procedure connected
+                                    // After this server will call the
+                                    // gotUserFriends function
+
+socket.on('gotUserFriends',function (friends){
+
+    /** To be done by tamim **/
+    displayUserFriends(friends);
+
+
 });
 
-socket.on('empty', function (room){
-  isInitiator = true;
-  console.log('Room ' + room + ' is empty');
-});
 
-socket.on('join', function (room){
-  console.log('Making request to join room ' + room);
-  console.log('You are the initiator!');
-});
 
-socket.on('log', function (array){
-  console.log.apply(console, array);
-});
+
+function displayUserFriends(friends)
+{
+
+    /** To be done by masum vai **/
+    /**
+        friends is a JSON object corresponding to the friends of the
+        current user with username global variable and which of them
+        are online.
+
+        The format of the JSON object is like this suppose the username
+        is tamim
+        friends is a string array containing the names
+        online is a boolean array denoting if the friend is online or not
+
+
+        {
+            'friends': ['masum','mahfuz','rakib','tuhin']
+            'online' : [1,0,0,1]
+
+
+        }
+
+        Use this info to update the UI
+    **/
+
+}
+
+function makeOffline(username)
+{
+    /** To be done by masum vai **/
+    /** Update the UI so that the username is set to offline **/
+
+
+
+}
+function makeOnline(username)
+{
+    /** To be done by masum vai **/
+    /**Update the UI so that the username is set to online **/
+
+}
+
+
--- server/server.js
+++ server/server.js
@@ -7,43 +7,113 @@ var app = http.createServer(function (req, res) {
 
 var io = require('socket.io').listen(app);
 
-io.sockets.on('connection', function (socket){
 
-  // convenience function to log server messages on the client
-	function log(){
-		var array = [">>> Message from server: "];
-	  for (var i = 0; i < arguments.length; i++) {
-	  	array.push(arguments[i]);
-	  }
-	    socket.emit('log', array);
-	}
+function makeOnline(username,socket)
+{
+    /** To be done by rakib **/
+    /*
+        This function makes the user with the username online
+        this also gets a socket variable so that u can make user
+        offline
 
-	socket.on('message', function (message) {
-		log('Got message:', message);
-    // for a real app, would be room only (not broadcast)
-		socket.broadcast.emit('message', message);
-	});
+    */
 
-	socket.on('create or join', function (room) {
-		var numClients = io.sockets.clients(room).length;
 
-		log('Room ' + room + ' has ' + numClients + ' client(s)');
-		log('Request to create or join room ' + room);
 
-		if (numClients === 0){
-			socket.join(room);
-			socket.emit('created', room);
-		} else if (numClients === 1) {
-			io.sockets.in(room).emit('join', room);
-			socket.join(room);
-			socket.emit('joined', room);
-		} else { // max two clients
-			socket.emit('full', room);
-		}
-		socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
-		socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+}
 
-	});
+function makeOffline(socket)
+{
+    /** To be done by rakib **/
+    /*
+        This function makes the user with the socket offline
+        and also returns the username of the user belonging to
+        this socket
+    */
+}
+
+function isOnline(username)
+{
+    /** To be done by rakib **/
+    /*
+         Checks if the user with username is online
+    */
+
+}
+function getFriends(username)
+{
+    /** To be done by rakib **/
+    /*
+        This function collects the username of the friends of the user
+        and then return an ARRAY of usernames which are his friends
+
+        use-case:
+            getFriends('tamim')
+            returns ['mahfuz','rakib','tuhin','masum']
+
+    */
+}
+
+function getSocket(username)
+{
+    /** To be done by rakib **/
+    /** Returns the socket corresponding to the current username **/
+
+
+}
+
+function getUsername(socket)
+{
+        /** To be done by rakib **/
+    /** Returns the username corresponding to the current socket **/
+
+}
+
+
+function userInCall() //Declaration to be deteremined
+
+
+
+
+
+
+
+io.sockets.on('connection', function (socket)
+{
+    socket.on('connected',function(username){
+        /**
+            todo by mahfuz , when a new user is connected,
+            notify each of users friend about that he is
+            online.
+            Set a socket aside for him
+
+
+        **/
+
+
+    });
+
+    socket.on('disconnect', function() {
+      /** To be done by tamim and mahfuz **/
+      console.log('Got disconnect!');
+
+      disconnectedUser = makeOffline(socket);
+      friends = getFriends(disconnectedUser);
+      // Code to call
+
+
+
+   });
+
+
+
+
 
 });
+
+
+
+
+
+
 
